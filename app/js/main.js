0ffd5b6de754e07f20347b6dfb248e09e6e80200
Yulia <46112108+Ju-nova@users.noreply.github.com>
2019-08-12 12:01:03 +0300

    добавлены основные

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+$(function() {
+	var tab = $('a[data-toggle="tab"]');
+
+	tab.on('click', function() {
+		event.preventDefault();
+		$('.submenu__list-item--current').removeClass('submenu__list-item--current');
+		$(this).parent().toggleClass('submenu__list-item--current');
+		var target = $(this).attr('href');
+		$('.active').removeClass('active');
+		$(target).toggleClass('active');
+	});
+    
+    var block = $('a[data-toggle="block"]');
+ 
+    block.hover(function(){
+
+   
+      var part = $(this).attr('href');
+      $('.submenu__block').removeClass('active');
+      $(part).addClass('active');        // навели курсор на объект (не учитываются переходы внутри элемента)         
+        },
+     
+      //   function(){ 
+      //    var part = $(this).attr('href');          
+      // $(part).removeClass('active');    // отвели курсор с объекта (не учитываются переходы внутри элемента)            
+      //   }
+      );
+
+    
+    });
+$(window).on("scroll", function () {
+    var scrolled = $(this).scrollTop();
+    if( scrolled > 132) {
+        $('.logo1').addClass('none');
+         $('.logo2').removeClass('none');
+         $('.header').addClass('header__scrolled');
+          $('.header__first-work').addClass('none');
+    }   
+    if( scrolled <= 132) {     
+       $('.logo1').removeClass('none');
+         $('.logo2').addClass('none');
+         $('.header').removeClass('header__scrolled');
+          $('.header__first-work').removeClass('none');
+    }
+});
